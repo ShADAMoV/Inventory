2f0cfbab08595417e36a467a2cea6b60d2f7d2f9
Adam <adam.shad@mail.ru>
2023-06-20 13:27:42 +0300

    shared layout end

---
+++ src/components/5shared/IBlurImage.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="i-blur-image">
+    <img
+      src="@/assets/images/blurImage.png"
+      alt="blurImage"
+    >
+  </div>
+</template>
+
+<style lang="scss">
+  .i-blur-image {
+    background: rgba(255, 255, 255, 0.1);
+    backdrop-filter: blur(6px);
+    border-radius: 8px;
+  }
+</style>
---
+++ src/components/5shared/ISkeleton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+interface Props {
+  width?: number
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <div :style=`"width:"${width}px` class="i-skeleton" />
+</template>
+
+<style lang="scss">
+  .i-skeleton {
+    background: linear-gradient(90deg, #3C3C3C 0%, #444444 51.04%, #333333 100%);
+    border-radius: 8px;
+    width: 100%;
+  }
+</style>
